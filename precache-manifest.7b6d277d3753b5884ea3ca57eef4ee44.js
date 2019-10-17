a19cf748e0807c3146cb164c3f0d2b44123bdc1b
Kossy <kossyb20@gmail.com>
2019-10-17 12:14:39 +0100

    Updates

---
+++ precache-manifest.7b6d277d3753b5884ea3ca57eef4ee44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97db262e8a10177aaf7f016b38a63a8e",
+    "url": "/create-react-app/index.html"
+  },
+  {
+    "revision": "a80defad5901b7082b03",
+    "url": "/create-react-app/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "72bf1bd3a12b510e2961",
+    "url": "/create-react-app/static/js/2.5b81f9ee.chunk.js"
+  },
+  {
+    "revision": "a80defad5901b7082b03",
+    "url": "/create-react-app/static/js/main.88593a7c.chunk.js"
+  },
+  {
+    "revision": "fa4fa99e9519a7aacdaa",
+    "url": "/create-react-app/static/js/runtime-main.b69a2cea.js"
+  }
+]);
